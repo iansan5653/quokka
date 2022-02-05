ff8fea4810e4b796ed4b229a411eb5abd9ba7f97
Ian Sanders <iansan5653@gmail.com>
2022-02-05 06:09:27 +0000

    Change default status to OOO

--- src/properties.ts
+++ src/properties.ts
@@ -1,7 +1,7 @@
 import { Status } from "./github/status";
 
 export const EVENT_BATCH_DURATION_HOURS = 24;
-export const DEFAULT_STATUS: Status = { message: "In a meeting", emoji: "calendar", busy: false };
+export const DEFAULT_STATUS: Status = { message: "Out of office", emoji: "beach", busy: true };
 export const EVENT_ADJACENCY_TOLERANCE_MINUTES = 5;
 // Users are limited to twenty scheduled triggers, and we need room for rebatch, sync, and debounce triggers
 export const MAX_BATCH_SIZE = 15
